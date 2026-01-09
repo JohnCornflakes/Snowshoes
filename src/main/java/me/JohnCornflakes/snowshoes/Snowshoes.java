b7103c352791563a2047ba8ef6d1524a066eb250
JohnCornflakes <johncornflakes@yahoo.com>
2026-01-09 04:44:58 -0800

    Initial commit

---
+++ src/main/java/me/JohnCornflakes/snowshoes/Snowshoes.java
@@ -0,0 +1,10 @@
+package me.JohnCornflakes.snowshoes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Snowshoes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/JohnCornflakes/snowshoes/client/SnowshoesClient.java
@@ -0,0 +1,10 @@
+package me.JohnCornflakes.snowshoes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SnowshoesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
